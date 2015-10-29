006a85cbb103a1eb45a27869dfebbffd9fa174e3
Heath Carroll <carrollh@email.sc.edu>
2015-10-29 08:37:00 -0400

    debugging from the ground up

--- datakeeper-cluster/Scripts/PrepareDataKeeperNodes.ps1
+++ datakeeper-cluster/Scripts/PrepareDataKeeperNodes.ps1
@@ -16,17 +16,16 @@ param
     [int] $NodeIndex	
 )
 
-function TraceInfo($log)
-{
-     "$(Get-Date -format 'MM/dd/yyyy HH:mm:ss') $log" | Add-Content -Confirm:$false $logFile 
-}
+
 
 Set-StrictMode -Version 3
 $datetimestr = (Get-Date).ToString("yyyyMMddHHmmssfff")        
 $logFile = "$env:windir\Temp\datakeeperInstallLog-$datetimestr.txt"
 
-TraceInfo "PrepareDataKeeperNodes script called with license:"
-TraceInfo $TempLicense
+function TraceInfo($log)
+{
+     "$(Get-Date -format 'MM/dd/yyyy HH:mm:ss') $log" | Add-Content -Confirm:$false $logFile 
+}
 
 $AdminPassword = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($AdminBase64Password))
 $domainNetBios = $DomainFQDN.Split(".")[0].ToUpper()
@@ -63,61 +62,5 @@ TraceInfo "Start to set initialize data disk(s)"
 Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru |	New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$false
 TraceInfo "Finished formatting data disk(s)"
 
-TraceInfo "Installing DataKeeper license"
-Add-Content $env:windir\SysWOW64\LKLicense\extmirrsvc.lic $TempLicense
-TraceInfo "Finished installing DataKeeper license"
-
-TraceInfo "Enabling and Configuring WSFC"
-Install-WindowsFeature -Name Failover-Clustering -IncludeManagementTools
-Import-Module FailoverClusters
-# TODO: add node to cluster 
-TraceInfo "Finished Configuring WSFC"
-
-TraceInfo "Verifying ExtMirr Service is Running"
-while(-Not $(Test-Path $env:windir\SysWOW64\LKLicense\extmirrsvc.lic)) {
-		Start-Sleep -Seconds 5
-}
-Restart-Service extmirrsvc
-Start-Sleep -Seconds 10
-if(-Not $(Get-Service extmirrsvc).Status -eq "Running") {
-	TraceInfo "ExtMirr Service failed to start. Mirror not created."
-	return 1
-}
-
-TraceInfo "Enabling remote script execution between nodes."
-Enable-PSRemoting -force
-winrm s winrm/config/client '@{TrustedHosts="sios-0,sios-1"}'
-
-Restart-Service winrm
-Start-Sleep -Seconds 10
-
-if(-Not $(Get-Service extmirrsvc).Status -eq "Running") {
-	TraceInfo "Remote Windows Management failed to start."
-	return 1
-}
-
-if($NodeIndex -eq 0) {	
-	TraceInfo "Waiting on sios-1 to start extmirrsvc"
-	while($true) {
-		if($(Get-Service extmirrsvc -ComputerName "sios-1").Status -eq "Running") {
-			break
-		} else {
-			Start-Sleep 10
-		}
-	}
-
-	TraceInfo "Creating mirror on volume F"
-	New-DataKeeperJob "Volume F" "Initial Mirror" "sios-0" "10.0.0.5" "F" "sios-1" "10.0.0.6" "F" "Async"
-	New-DataKeeperMirror "10.0.0.5" "F" "10.0.0.6" "F" "Async"
-	
-	# TODO: register mirror with cluster
-	
-	TraceInfo "Finished creating mirror. Restarting remote node (sios-1)."
-	Restart-Computer sios-1 -force
-	
-	TraceInfo "Restarting this node (sios-0) after 30 seconds"
-	Start-Process -FilePath "cmd.exe" -ArgumentList "/c shutdown /r /t 30"
-
-} else {
-	TraceInfo "Completed provisioning sios-1, waiting on sios-0 to restart this node."
-}
+TraceInfo "Restart after 30 seconds"
+Start-Process -FilePath "cmd.exe" -ArgumentList "/c shutdown /r /t 30"
