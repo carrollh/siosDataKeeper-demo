77145a9d4e6901a023d46f10f456e22207da6bfd
Heath Carroll <carrollh@email.sc.edu>
2015-11-02 10:56:02 -0500

    testing ftp get

--- test/test.ps1
+++ test/test.ps1
@@ -7,36 +7,38 @@ param
     [String] $Base64Password,
 		
 		[Parameter(Mandatory=$true)]
-    [String] $TempLicense
+    [String] $LicenseKeyFtpURL
 )
+
+$logFile = "$env:windir\Temp\PrepareDataKeeperNode_log-$datetimestr.txt"
+$licFile = "$env:windir\SysWOW64\LKLicense\extmirrsvc.lic"
 
 function TraceInfo($log)
 {
-     "$(Get-Date -format 'MM/dd/yyyy HH:mm:ss') $log" | Add-Content -Confirm:$false $logFile 
+  "$(Get-Date -format 'MM/dd/yyyy HH:mm:ss') $log" | Add-Content -Confirm:$false $logFile 
+}
+
+function Add-InitialMirror {
+	TraceInfo "MIRROR CREATED!!!"
 }
 
 Set-StrictMode -Version 3
 $datetimestr = (Get-Date).ToString("yyyyMMddHHmmssfff")        
-$logFile = "$env:windir\Temp\PrepareDataKeeperNode_log-$datetimestr.txt"
 
-$license = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($TempLicense))
-TraceInfo "PrepareDataKeeperNodes script called with TempLicense:"
-TraceInfo $TempLicense
-TraceInfo "TempLicense converted to: "
-TraceInfo $license
+TraceInfo "Downloading license file."
+Invoke-WebRequest $LicenseKeyFtpURL -OutFile $licFile
+
+if($(Test-Path $licFile)) {
+	TraceInfo "License file downloaded successfully."
+	Restart-Service extmirrsvc
+	Add-InitialMirror
+} else {
+	TraceInfo "Download FAILED, license not obtained."
+}
 
 TraceInfo "Start to set initialize data disk(s)"
 Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru |	New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$false
 TraceInfo "Finished formatting data disk(s)"
-
-
-TraceInfo "Installing DataKeeper license"
-$licfile = "$env:windir\SysWOW64\LKLicense\extmirrsvc.lic"
-Add-Content $licfile $license
-TraceInfo "Finished installing DataKeeper license"
-
-
-
 	
 TraceInfo "Restarting this node after 30 seconds"
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c shutdown /r /t 30"
