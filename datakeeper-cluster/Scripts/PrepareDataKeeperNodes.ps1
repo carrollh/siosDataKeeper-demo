9bf659d55b372d03e41f35154d957b9bc8ae7720
Heath Carroll <carrollh@email.sc.edu>
2015-11-03 11:39:26 -0500

    found the bug, I think

--- datakeeper-cluster/Scripts/PrepareDataKeeperNodes.ps1
+++ datakeeper-cluster/Scripts/PrepareDataKeeperNodes.ps1
@@ -41,7 +41,7 @@ $domainRole = (Get-WmiObject Win32_ComputerSystem).DomainRole
 TraceInfo "Domain role $domainRole"
 if($domainRole -ne 3)
 {
-    TraceInfo "$env:COMPUTERNAME does not join the domain, start to join domain $DomainFQDN"
+    TraceInfo "$env:COMPUTERNAME is not in a domain. Joining domain $DomainFQDN"
     # join the domain
     while($true)
     {
@@ -62,6 +62,46 @@ if($domainRole -ne 3)
     }
 }
 
-TraceInfo "Start to set initialize data disk(s)"
+TraceInfo "Starting to initialize data disk(s)"
 Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru |	New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$false
 TraceInfo "Finished formatting data disk(s)"
+
+TraceInfo "Downloading license file."
+$licFile = ""
+# check to see if the user pasted in a different license file, and preserve it's name / use it
+# this also works if the user navigated to the link first and pasted in the full file path url 
+if($LicenseKeyFtpURL.EndsWith(".lic")) {
+	$licFile = $LicenseKeyFtpURL.Substring($LicenseKeyFtpURL.LastIndexOf("/"))
+	Invoke-WebRequest $LicenseKeyFtpURL -OutFile ($licFolder+$licFile)
+} else { # otherwise use the standard file name
+	$licFile = "/DK-W-Cluster.lic"
+	Invoke-WebRequest ($LicenseKeyFtpURL+$licFile) -OutFile ($licFolder+$licFile)
+}
+
+if($(Test-Path ($licFolder+$licFile))) {
+	TraceInfo "License file downloaded successfully."
+	Restart-Service extmirrsvc
+	Add-InitialMirror
+} else {
+	TraceInfo "Download FAILED, license not obtained."
+}
+
+TraceInfo "Downloading license file."
+$licFile = ""
+# check to see if the user pasted in a different license file, and preserve it's name / use it
+# this also works if the user navigated to the link first and pasted in the full file path url 
+if($LicenseKeyFtpURL.EndsWith(".lic")) {
+	$licFile = $LicenseKeyFtpURL.Substring($LicenseKeyFtpURL.LastIndexOf("/"))
+	Invoke-WebRequest $LicenseKeyFtpURL -OutFile ($licFolder+$licFile)
+} else { # otherwise use the standard file name
+	$licFile = "/DK-W-Cluster.lic"
+	Invoke-WebRequest ($LicenseKeyFtpURL+$licFile) -OutFile ($licFolder+$licFile)
+}
+
+if($(Test-Path ($licFolder+$licFile))) {
+	TraceInfo "License file downloaded successfully."
+	Restart-Service extmirrsvc
+	Add-InitialMirror
+} else {
+	TraceInfo "Download FAILED, license not obtained."
+}
