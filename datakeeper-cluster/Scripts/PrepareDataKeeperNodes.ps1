b579964c8c775e375cd05b325108738c94f21f08
Heath Carroll <carrollh@email.sc.edu>
2015-11-03 10:35:01 -0500

    previous version deploys (even in VS), adding half of remainder to narrow down the bug

--- datakeeper-cluster/Scripts/PrepareDataKeeperNodes.ps1
+++ datakeeper-cluster/Scripts/PrepareDataKeeperNodes.ps1
@@ -28,4 +28,40 @@ function Add-InitialMirror {
 	TraceInfo "MIRROR CREATED!!!"
 }
 
-Add-InitialMirror
+Set-StrictMode -Version 3
+$datetimestr = (Get-Date).ToString("yyyyMMddHHmmssfff")        
+
+$AdminPassword = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($AdminBase64Password))
+$domainNetBios = $DomainFQDN.Split(".")[0].ToUpper()
+$domainUserCred = New-Object -TypeName System.Management.Automation.PSCredential `
+        -ArgumentList @("$domainNetBios\$AdminUserName", (ConvertTo-SecureString -String $AdminPassword -AsPlainText -Force))
+
+# 0 for Standalone Workstation, 1 for Member Workstation, 2 for Standalone Server, 3 for Member Server, 4 for Backup Domain Controller, 5 for Primary Domain Controller
+$domainRole = (Get-WmiObject Win32_ComputerSystem).DomainRole
+TraceInfo "Domain role $domainRole"
+if($domainRole -ne 3)
+{
+    TraceInfo "$env:COMPUTERNAME does not join the domain, start to join domain $DomainFQDN"
+    # join the domain
+    while($true)
+    {
+        try
+        {
+            Add-Computer -DomainName $DomainFQDN -Credential $domainUserCred -ErrorAction Stop
+            TraceInfo "Joined to the domain $DomainFQDN."
+            break
+        }
+        catch
+        {
+            TraceInfo "Join domain failed, will try after 10 seconds, $_"
+            # Flush the DNS cache in case the cached head node ip is wrong.
+            # Do not use Clear-DnsClientCache because it is not supported in Windows Server 2008 R2
+            Start-Process -FilePath ipconfig -ArgumentList "/flushdns" -Wait -NoNewWindow | Out-Null
+            Start-Sleep -Seconds 10
+        }
+    }
+}
+
+TraceInfo "Start to set initialize data disk(s)"
+Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru |	New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$false
+TraceInfo "Finished formatting data disk(s)"
